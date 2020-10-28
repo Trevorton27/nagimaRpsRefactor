158b5e7fb08ee16a6513c9a38fef03728e34b986
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-27 23:11:06 -0400

    Second step

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-function DoSomething() {
-    alert("Hello, this is an alert. Thanks, Andy :) ");
-  }
-
-
-
-
-
-
+function DoSomething(x) {
+ if(x==0) {
+ alert("YOU WON ");
+}else if (x==1)
+alert("YOU LOST")
+else
+alert("IT'S A TIE")
+}
