d0b390eb40bc009de51904f2f13b163bd63b84a7
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-30 17:13:33 -0400

    Changes

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function DoSomething(x) {
- if(x==0) {
+function DoSomething(x, y) {
+ if(x==0)  {
  alert("YOU WON ");
 }else if (x==1)
 alert("YOU LOST")
 else
 alert("IT'S A TIE")
 }
+
+const computerchoice = Math.floor((Math.random() * 3) + 1);
+console.log(computerchoice);
