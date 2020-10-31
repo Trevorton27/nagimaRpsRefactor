846213521a2c75675c9c4d774f00094c1e70be17
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-31 12:10:46 -0400

    Changes

--- main.js
+++ main.js
@@ -14,40 +14,40 @@ function computer_choice() {
   
   
   function DoSomething(yourchoice) {
-    yourchoice = ['rock', 'paper', 'scissors'];
+    // yourchoice = ['rock', 'paper', 'scissors'];
     robotpick = computer_choice();
   
     // compare for tie
     if (yourchoice == robotpick) {
       resultstxt = 'TIE!'
-      // alert("IT'S A TIE !!! . Computer's choice - " + robotpick);
+      alert("IT'S A TIE !!! . Computer's choice - " + robotpick);
     }
     // compare yourchoice ROCK to computers choice
     else if (yourchoice == "rock" && robotpick == "paper") {
       resultstxt = 'LOSER!'
-      // alert("YOU LOST. Computer's choice - " + robotpick);
+      alert("YOU LOST. Computer's choice - " + robotpick);
     } 
     else if (yourchoice == "rock" && robotpick == "scissors") {
       resultstxt = 'WINNER!'
-      // alert("YOU WON. Computer's choice - " + robotpick);
+      alert("YOU WON. Computer's choice - " + robotpick);
     }
     //compare yourchoice PAPER to computers choice
     else if (yourchoice == "paper" && robotpick == "rock") {
       resultstxt = 'WINNER!'
-  //    alert("YOU WON vs " + robotpick);
+    alert("YOU WON vs " + robotpick);
     } 
     else if (yourchoice == "paper" && robotpick == "scissors") {
       resultstxt = 'LOSER!'
-      // alert("YOU LOST. Computer's choice -  " + robotpick);
+      alert("YOU LOST. Computer's choice -  " + robotpick);
     }
     //compare yourchoice SCISSORS to computers choice
     else if (yourchoice == "scissors" && robotpick == "rock") {
       resultstxt = 'LOSER!'
-      // alert("YOU LOST. Computer's choice - " + robotpick);
+      alert("YOU LOST. Computer's choice - " + robotpick);
     } 
     else if (yourchoice == "scissors" && robotpick == "paper") {
       resultstxt = 'WINNER!'
-        // alert("YOU WON. Computer's choice - " + robotpick);
+      alert("YOU WON. Computer's choice - " + robotpick);
     }
   return robotpick;
   }
