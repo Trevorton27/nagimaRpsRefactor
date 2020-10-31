bdbf822deb7123eaccf626c1216efec2b30a0b79
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-30 23:55:33 -0400

    Adding computer's random choice

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-function DoSomething(x, y) {
- if(x==0)  {
- alert("YOU WON ");
-}else if (x==1)
-alert("YOU LOST")
-else
-alert("IT'S A TIE")
-}
-
-<<<<<<< HEAD
-const computerchoice = Math.floor((Math.random() * 3) + 1);
-console.log(computerchoice);
-=======
-// hello, I am just testing this file
->>>>>>> bf9f6e720528069f66004a75e4b54b3bab4ba1e0
+function computer_choice() {
+    choice = Math.random();
+    if (choice <= 0.33) {
+      choice = "rock";
+    } else if (choice <= 0.66) {
+      choice = "paper";
+    } else {
+      choice = "scissors";
+    }
+    console.log(choice);
+    return choice;
+  }
+  
+  
+  
+  function DoSomething(yourchoice) {
+    yourchoice = ['rock', 'paper', 'scissors'];
+    robotpick = computer_choice();
+  
+    // compare for tie
+    if (yourchoice == robotpick) {
+      resultstxt = 'TIE!'
+      // alert("IT'S A TIE !!! . Computer's choice - " + robotpick);
+    }
+    // compare yourchoice ROCK to computers choice
+    else if (yourchoice == "rock" && robotpick == "paper") {
+      resultstxt = 'LOSER!'
+      // alert("YOU LOST. Computer's choice - " + robotpick);
+    } 
+    else if (yourchoice == "rock" && robotpick == "scissors") {
+      resultstxt = 'WINNER!'
+      // alert("YOU WON. Computer's choice - " + robotpick);
+    }
+    //compare yourchoice PAPER to computers choice
+    else if (yourchoice == "paper" && robotpick == "rock") {
+      resultstxt = 'WINNER!'
+  //    alert("YOU WON vs " + robotpick);
+    } 
+    else if (yourchoice == "paper" && robotpick == "scissors") {
+      resultstxt = 'LOSER!'
+      // alert("YOU LOST. Computer's choice -  " + robotpick);
+    }
+    //compare yourchoice SCISSORS to computers choice
+    else if (yourchoice == "scissors" && robotpick == "rock") {
+      resultstxt = 'LOSER!'
+      // alert("YOU LOST. Computer's choice - " + robotpick);
+    } 
+    else if (yourchoice == "scissors" && robotpick == "paper") {
+      resultstxt = 'WINNER!'
+        // alert("YOU WON. Computer's choice - " + robotpick);
+    }
+  return robotpick;
+  }
